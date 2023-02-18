f49892310dfb3bc371b00b96c9dc5b59590be0fb
nlpquan <nguyenlephuongquan@gmail.com>
2023-02-17 20:57:24 -0700

    fix getmapping

--- src/main/java/com/gcu/LoginController.java
+++ src/main/java/com/gcu/LoginController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 	
 	@Autowired
