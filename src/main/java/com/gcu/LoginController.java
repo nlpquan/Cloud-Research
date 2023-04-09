2f1a6daae3d88be0ba809f4c93c38867844fe185
Quan Nguyen <56704320+nlpquan@users.noreply.github.com>
2023-04-09 11:40:23 -0700

    Add files via upload

--- src/main/java/com/gcu/LoginController.java
+++ src/main/java/com/gcu/LoginController.java
@@ -1,17 +1,25 @@
 package com.gcu;
 
+import java.util.ArrayList;
+
+import java.util.List;
 
 import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+	
+	Logger logger = LoggerFactory.getLogger(LoggingController.class);
 	
 	@Autowired
 	private OrdersBusinessServiceInterface service;
@@ -36,7 +44,13 @@ public class LoginController {
 		
 		model.addAttribute("title", "My Orders");
 		model.addAttribute("orders", service.getOrders());
-				
+		
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+		
 		return "orders";
 	}
 }
